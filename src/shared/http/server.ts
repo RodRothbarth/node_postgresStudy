78d51b4d47c8516aa2794ad85f891f6c5d5024a0
rodrothbarth <rodrothbarth@gmail.com>
2024-09-27 11:48:48 -0300

    feat base error handler

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,29 @@
-console.log("hello world")
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from '@shared/routes';
+import AppError from '@shared/error/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Ligado');
+});
